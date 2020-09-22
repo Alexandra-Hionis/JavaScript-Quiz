ba9b7236b9356da957ec1374e8e830fd2574f465
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-09-21 20:43:16 -0400

    update

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
 
 
-var timeEl = document.querySelector(".count");
-var mainEl = document.getElementById("timer");
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
+ var timeEl = document.querySelector(".count");
+ var mainEl = document.getElementById("timer");
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
 
-function makeQuiz(){
-    const myQuestions = [
-        {
-          question: "Inside which HTML element do we put the JavaScript?",
-          answers: ["<script>", "<javascript>", "<scripting>", "<js>"],
-          correctAnswer: 0
-        },
-        {
-          question: "Where is the correct place to insert a JavaScript?",
-          answers: ["<body>", "<head> and <body>", "<head>", "<footer>"],
-          correctAnswer: 1
-        },
-        {
-          question: "How do you write \"Hello World\" in an alert box??",
-          answers: {
-            "(\"Hello World\")",
-            "alertBox(\"Hello World\")",
-            "alert(\"Hello World\")",
-            "console.log(\"Hello World\")"
-          },
-          correctAnswer: "d"
-        }
-        {
-            question: "How does a FOR loop start?",
-            answers: {
-              "for(i = 0; i <= 5)",
-              "for i = 1 to 5",
-              "for(i=0; i <=5; i++",
-              "while (i <= 5; i++)"
-            },
-            correctAnswer: "c"
+  function makeQuiz(){
+
+    document.getElementById("start-button").click(); 
+      const myQuestions = [
+          {
+            question: "Inside which HTML element do we put the JavaScript?",
+            answers: ["<script>", "<javascript>", "<scripting>", "<js>"],
+            correctAnswer: 0
           },
           {
-            question: "How do you create a function in JavaScript?",
-            answers: {
-              "function = myFunction()",
-              "function:myFunction()",
-              "function my function",
-              "function()"
-            },
-            correctAnswer: "c"
+            question: "Where is the correct place to insert a JavaScript?",
+            answers: ["<body>", "<head> and <body>", "<head>", "<footer>"],
+            correctAnswer: 1
           },
-      ];
-}
+          {
+            question: "How do you write \"Hello World\" in an alert box??",
+            answers: [ "(\"Hello World\")", "alertBox(\"Hello World\")", "alert(\"Hello World\")", "console.log(\"Hello World\")"],
+            correctAnswer: 2
+          }
+          {
+              question: "How does a FOR loop start?",
+              answers: ["for(i = 0; i <= 5)", "for i = 1 to 5", "for(i=0; i <=5; i++)", "while (i <= 5; i++)"],
+              correctAnswer: 2
+            },
+            {
+              question: "How do you create a function in JavaScript?",
+              answers: ["function = myFunction()", "function()", "function my function", "function myFunction(a,b)",],
+              correctAnswer: 3
+            },
+        ];
+  }
 //WHEN START BUTTON IS PRESSED, FIRST QUESTION APPEARS AND TIMER STARTS
 
 // var secondsLeft = 75; 
