b05113aa6827fc3437c956c380f9bb23f75f1200
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-09-25 13:34:36 -0400

    trying to update high scores page

--- assets/highscores.js
+++ assets/highscores.js
@@ -3,6 +3,11 @@
 var highScore = document.querySelector("#hsList");
 var clear = document.querySelector("#clearButton");
 var goBack = document.querySelector("#goBackButton");
+const initialsInput = document.getElementById("initialsInput");
+const initials = document.getElementById("initials");
+const score = 0; // Score starts off at 0
+var secondsLeft = 75;
+var timerInterval = null;
 
 // Event listener to clear scores 
 clear.addEventListener("click", function () {
@@ -10,9 +15,16 @@ clear.addEventListener("click", function () {
     location.reload();
 });
 
+// Retreives local stroage 
+var highScores = localStorage.getItem("initials", "score");
+highScores = JSON.parse(highScores);
 
-    
+if (highScores !== null) {
 
+
+    }
+
+   
 // Event listener to move to index page
 goBack.addEventListener("click", function () {
     window.location.replace("index.html");
