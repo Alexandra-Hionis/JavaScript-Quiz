75b9a37bac82edc2927466fc32a06110a8f94eff
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-09-25 12:25:46 -0400

    finally got timer to equal the score, enter initials working, and set up local storage

--- assets/script.js
+++ assets/script.js
@@ -19,9 +19,42 @@ const endContainerElement = document.getElementById("endCard")
 const playAgainBtn = document.getElementById("playAgainButton");
 const clearBtn = document.getElementById("clearButton");
 const mainEl = document.getElementById("timer");
+const initialsInput = document.getElementById("initialsInput");
+const initials = document.getElementById("initials");
+var timerInterval = null;
 
 const choiceButtonTwo = document.getElementsByClassName("btn2");
 console.log("button", choiceButtonTwo);
+
+// Add listeners to answer buttons
+for (let index = 0; index < choiceButtonTwo.length; index++) {
+    const element = choiceButtonTwo[index];
+    element.addEventListener("click", function (e) {
+        var q = quizQuestions[currentQuestion]
+        // console.log("this is working")
+        // console.log(e, e.target, e.target.innerHTML)
+        quizQuestions[currentQuestion].userAnswer = e.target.innerHTML
+        var correctAnswer = q["choice" + q.correctAnswer]
+        var userAnswer = e.target.innerHTML
+
+        if (userAnswer !== correctAnswer) {
+            secondsLeft = secondsLeft - 10
+            timeEl.textContent = "Timer: " + secondsLeft;
+        }
+
+        // Go to next question
+        currentQuestion++
+
+        if(currentQuestion === quizQuestions.length) {
+            scoreQuiz();
+        } else {
+            getQuestion(currentQuestion)
+        }
+
+        
+    })
+
+}
 
 // querySelector
 const timeEl = document.querySelector(".count");
@@ -33,11 +66,7 @@ questionContainerElement.style.display = "none";
 // Hide score section on instruction page
 scoreContainerElement.style.display = "none";
 
-// Hide play again button on instuction page
-playAgainBtn.style.display = "none";
 
-// Hide clear highscores button on instruction page
-clearBtn.style.display = "none";
 
 // Hide end section on instructions page
 endContainerElement.style.display = "none";
@@ -70,7 +99,7 @@ startButton.addEventListener("click", function () {
     timeEl.textContent = "Timer: " + secondsLeft;
 
 
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = "Timer: " + secondsLeft;
 
@@ -110,78 +139,52 @@ const quizQuestions = [
         correctAnswer: "2"
     },
 
-    // { // If questionIndex is 2, this object will be displayed
-    //     question: "How does a while loop start?",
-    //     choice1: "while( i<=10; i++",
-    //     choice2: "while i = 1 to 10",
-    //     choice3: "while(i == 5)",
-    //     choice4: "while(i <=10)",
-    //     correctAnswer: "4"
-    // },
-
-    // { // If questionIndex is 3, this object will be displayed
-    //     question: "Inside which HTML element do we put the JavaScript?",
-    //     choice1: "<script>",
-    //     choice2: "<javascript>",
-    //     choice3: "<scripting>",
-    //     choice4: "<js>",
-    //     correctAnswer: "1"
-    // },
-
-    // { // If questionIndex is 4, this object will be displayed
-    //     question: "How do you round the number 7.25, to the nearest integer?",
-    //     choice1: "Math.round(7.25)",
-    //     choice2: "rnd(7.25)",
-    //     choice3: "Math.rnd(7.25)",
-    //     choice4: "round(7.25)",
-    //     correctAnswer: "1"
-    // },
+    { // If questionIndex is 2, this object will be displayed
+        question: "How does a while loop start?",
+        choice1: "while( i<=10; i++",
+        choice2: "while i = 1 to 10",
+        choice3: "while(i == 5)",
+        choice4: "while(i <=10)",
+        correctAnswer: "4"
+    },
 
 
-    // { // If questionIndex is 5, this object will be displayed
-    //     question: "How to write an if statement?",
-    //     choice1: "if i == 5 then",
-    //     choice2: "IF i = 5 then",
-    //     choice3: "if (i = 5)",
-    //     choice4: "if(i == 5)",
-    //     correctAnswer: "4",
-    // },
+    { // If questionIndex is 3, this object will be displayed
+        question: "How to write an if statement?",
+        choice1: "if i == 5 then",
+        choice2: "IF i = 5 then",
+        choice3: "if (i = 5)",
+        choice4: "if(i == 5)",
+        correctAnswer: "4",
+    },
 
-    // { // If questionIndex is 6, this object will be displayed
-    //     question: "How to write an if statement for executing code that's NOT equal to 5?",
-    //     choice1: "if(! = 5)",
-    //     choice2: "if i = !",
-    //     choice3: "if i <! 5",
-    //     choice4: "if( i <> 5)",
-    //     correctAnswer: "1",
-    // },
+    { // If questionIndex is 4, this object will be displayed
+        question: "How to write an if statement for executing code that's NOT equal to 5?",
+        choice1: "if(! = 5)",
+        choice2: "if i = !",
+        choice3: "if i <! 5",
+        choice4: "if( i <> 5)",
+        correctAnswer: "1",
+    },
 
-    // { // If questionIndex is 7, this object will be displayed
-    //     question: "How does a for loop start?",
-    //     choice1: "for(i = 0; i <= 5)",
-    //     choice2: "for i = 1 to 5",
-    //     choice3: "for(i=0; i <=5; i++)",
-    //     choice4: "while (i <= 5; i++)",
-    //     correctAnswer: "3",
-    // },
+    { // If questionIndex is 5, this object will be displayed
+        question: "How does a for loop start?",
+        choice1: "for(i = 0; i <= 5)",
+        choice2: "for i = 1 to 5",
+        choice3: "for(i=0; i <=5; i++)",
+        choice4: "while (i <= 5; i++)",
+        correctAnswer: "3",
+    },
 
-    // { // If questionIndex is 8, this object will be displayed
-    //     question: "How do you create a function in JavaScript?",
-    //     choice1: "function = myFunction()",
-    //     choice2: "function()",
-    //     choice3: "function my function",
-    //     choice4: "function myFunction(a,b)",
-    //     correctAnswer: "4",
-    // },
+    { // If questionIndex is 6, this object will be displayed
+        question: "How do you create a function in JavaScript?",
+        choice1: "function = myFunction()",
+        choice2: "function()",
+        choice3: "function my function",
+        choice4: "function myFunction(a,b)",
+        correctAnswer: "4",
+    },
 
-    // { // If questionIndex is 9, this object will be displayed
-    //     question: "How do you write \"Hello World\" in an alert box??",
-    //     choice1: "(\"Hello World\")",
-    //     choice2: "alertBox(\"Hello World\")",
-    //     choice3: "alert(\"Hello World\")",
-    //     choice4: "console.log(\"Hello World\")",
-    //     correctAnswer: "3",
-    // }
 ];
 
 
@@ -197,55 +200,77 @@ function getQuestion(questionIndex) {
     choiceFour.textContent = q.choice4;
 
 
-
-
-
 };
 
 
-function nextQuestion() {
 
-};
-// quiz button listener
-for (let index = 0; index < choiceButtonTwo.length; index++) {
-    const element = choiceButtonTwo[index];
-    element.addEventListener("click", function (e) {
-        var q = quizQuestions[currentQuestion]
-        console.log("this is working")
-        console.log(e, e.target, e.target.innerHTML)
-        quizQuestions[currentQuestion].userAnswer = e.target.innerHTML
-        var correctAnswer = q["choice" + q.correctAnswer]
-        var userAnswer = e.target.innerHTML
-        if (userAnswer !== correctAnswer) {
-            // secondsLeft = secondsLeft - 10
-            secondsLeft -= 10
 
-        }
-// Go to next question
-        currentQuestion++
 
-        if(currentQuestion === quizQuestions.length) {
-            endQuiz();
-        }else {
-            getQuestion(currentQuestion)
-        }
-
-        
-    })
-
-}
 
 
 function checkAnswer() {
 
     if (questions[currentQuestions].answer == answer) { // If values are same, question is correct
-        addScore += 10; // Increase score by 10 if correct
-        console.log("correct")
+        console.log("Right!");
     }
     selectedChoice.clicked = false;
+    console.log ("Wrong!");
     currentQuestion++; // Incrementing current position number
     if (currentQuestion == totalQuestions - 1) { }
+    
+    
 }
+
+
+
+
+
+
+function scoreQuiz() {
+
+    // Hide instructions and button 
+    instructionsContainerElement.style.display = "none";
+
+    // Show questions section
+    questionContainerElement.style.display = "none";
+
+    scoreContainerElement.style.display = "block";
+    console.log(secondsLeft);
+
+    const finalScore = document.getElementById("final-score");
+    finalScore.innerText = "Your score: " + secondsLeft
+    
+    clearInterval(timerInterval);
+
+    
+}
+
+
+// At the end, user should be able to unput their initials in high score form and either click clear highscore or play again
+function endQuiz() {
+
+   let initialsText = initialsInput.value
+    localStorage.setItem(initialsText, secondsLeft);
+
+      // Hide instructions and button 
+      scoreContainerElement.style.display = "none";
+      //show
+      endContainerElement.style.display = "block";
+      
+    for (var i = 0; i < localStorage.length; i++){
+    let initialsText = localStorage.key(i);
+    let score = localStorage.getItem(initialsText);
+    let li = document.createElement("li");
+    li.innerHTML = `${initialsText}: ${score}`;
+    initials.appendChild(li);
+    }
+}
+
+okButton.addEventListener("click", function () {
+    endQuiz();
+
+})
+
 
 
 // Event listener to move to index page
@@ -260,19 +285,7 @@ playAgainBtn.addEventListener("click", function () {
 
 });
 
-function endQuiz() {
-
-    // Hide instructions and button 
-    instructionsContainerElement.style.display = "none";
-
-    // Show questions section
-    questionContainerElement.style.display = "none";
-
-    scoreContainerElement.style.display = "block";
-    
-}
-
-//create function that when correct button is pressed, score gains 10 points
-//if wrong, down 10 points
-
-//At the end, user should be able to unput their initials in high score form and either click clear hiscore or play again
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
